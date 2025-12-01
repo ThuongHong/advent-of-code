d3c75e2172249c49c6e43a716bff29e2de894037
Luu Thuong Hong <luuthuonghong@gmail.com>
2025-12-01 17:04:18 +0700

    [2025] day 1

---
+++ advent-of-code-2025/day_1/day1_p2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ifstream fi("day1.txt");
+    string line;
+
+    int cur = 50;
+    int password = 0;
+    while (fi >> line)
+    {
+        int rotated = 0; // 0 ~ left, 1 ~ right
+        if (line[0] == 'R')
+            rotated = 1;
+
+        line[0] = '0';
+        int dist = stoi(line);
+
+        password += dist / 100;
+        dist %= 100;
+        if (!rotated)
+        {
+            if (cur - dist >= 0)
+                cur -= dist;
+            else if (cur != 0)
+            {
+                cur = 100 - (dist - cur);
+                if (cur != 0)
+                {
+                    password += 1;
+                }
+            }
+            else
+                cur = 100 - dist;
+        }
+        else
+        {
+            if (dist + cur <= 99)
+                cur += dist;
+            else
+            {
+                cur = cur + dist - 100;
+                password += 1 * (cur != 0);
+            }
+        }
+        if (cur == 0)
+            password += 1;
+
+        cout << cur << "\n";
+    }
+    cout << "Password: " << password;
+
+    fi.close();
+}
